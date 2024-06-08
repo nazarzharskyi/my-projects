0ce69027da76955d3a3fef8350ce31e785783340
nazarzharskyi <150830258+nazarzharskyi@users.noreply.github.com>
2024-06-08 13:34:25 +0300

    111

---
+++ OnlineShop/OnlineShop/Entities/OrderSupply.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineShop.Entities
+{
+    internal class OrderSupply
+    {
+        public int OrderID { get; init; }
+        public int SupplierID { get; init; }
+        public int ProductID { get; set; }
+        public int ProductAmount { get; set; }
+        public string OrderTime { get; set; }
+        public int EmployeeID { get; set; }
+        public Product product { get; set; }
+        public OrderSupply(int orderID, int supplierID, int productID, int productAmount, string orderTime, int employeeID)
+        {
+            OrderID = orderID;
+            SupplierID = supplierID;
+            ProductID = productID;
+            ProductAmount = productAmount;
+            OrderTime = orderTime;
+            EmployeeID = employeeID;
+        }
+        public override string ToString()
+        {
+            return $"Order ID: {OrderID}, Supplier ID: {SupplierID}, Product ID: {ProductID}, Product Amount: {ProductAmount}, Order time: {OrderTime}, Employee ID: {EmployeeID}, Product: {product}";
+        }
+    } 
+}
---
+++ OnlineShop/OnlineShop/OrderFromSupplierList.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineShop
+{
+    internal class OrderFromSupplierList
+    {
+        public ListController<OrderFromSupplier> orderFromSup;
+        public OrderFromSupplierList()
+        {
+            orderFromSup = new ListController<OrderFromSupplier>();
+        }
+        public void OrderSupply(OrderFromSupplier order)
+        {
+            orderFromSup.AddItem(order);
+        }
+        public List<OrderFromSupplier> GetOrdersOfSupply()
+        {
+            return orderFromSup.GetItems();
+        }
+        public override string ToString()
+        {
+            Console.WriteLine(orderFromSup.ToString());
+            return orderFromSup.ToString();
+        }
+    }
+}
